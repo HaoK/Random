891f6e4344242c8b645389c27dad670ff79a8a9c
Hao Kung <HaoK@users.noreply.github.com>
2018-04-13 13:02:27 -0700

    Add repro for SignalR
    https://github.com/aspnet/SignalR/issues/2008

---
+++ WebApplication17/WebApplication17/Hubs/ChatHub.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.SignalR;
+
+namespace WebApplication17.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public override async Task OnConnectedAsync()
+        {
+            await Clients.All.SendAsync("SendAction", "Someone", "joined");
+        }
+
+        public override async Task OnDisconnectedAsync(Exception ex)
+        {
+            await Clients.All.SendAsync("SendAction", "Someone", "left");
+        }
+
+        public async Task Send(string message)
+        {
+            await Clients.All.SendAsync("SendMessage", "Someone", message);
+        }
+    }
+}
---
+++ WebApplication17/WinFormsSignalRClient/WinFormsClient.cs
@@ -0,0 +1,113 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsSignalRClient
+{
+    /// <summary>
+    /// SignalR client hosted in a WinForms application. The client
+    /// lets the user pick a user name, connect to the server asynchronously
+    /// to not block the UI thread, and send chat messages to all connected 
+    /// clients whether they are hosted in WinForms, WPF, or a web application.
+    /// </summary>
+    public partial class WinFormsClient : Form
+    {
+        /// <summary>
+        /// This name is simply added to sent messages to identify the user; this 
+        /// sample does not include authentication.
+        /// </summary>
+        private String UserName { get; set; }
+        const string ServerURI = "https://localhost:44336/hubs/chat";
+        private HubConnection Connection { get; set; }
+
+        internal WinFormsClient()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonSend_Click(object sender, EventArgs e)
+        {
+            Connection.InvokeAsync("Send", UserName, TextBoxMessage.Text).Wait();
+            TextBoxMessage.Text = String.Empty;
+            TextBoxMessage.Focus();
+        }
+
+        /// <summary>
+        /// Creates and connects the hub connection and hub proxy. This method
+        /// is called asynchronously from SignInButton_Click.
+        /// </summary>
+        private async void ConnectAsync()
+        {
+            Console.WriteLine("Connecting to {0}", ServerURI);
+
+            Connection = new HubConnectionBuilder().WithUrl(ServerURI).Build();
+            Connection.Closed += Connection_Closed;
+            //HubProxy = Connection.Create("MyHub");
+            //Handle incoming event from server: use Invoke to write to console from SignalR's thread
+            Connection.On<string, string>("Send", (name, message) =>
+                this.Invoke((Action)(() =>
+                    RichTextBoxConsole.AppendText(String.Format("{0}: {1}" + Environment.NewLine, name, message))
+                ))
+            );
+            try
+            {
+                await Connection.StartAsync();
+            }
+            catch (HttpRequestException)
+            {
+                StatusText.Text = "Unable to connect to server: Start server before connecting clients.";
+                //No connection: Don't enable Send button or show chat UI
+                return;
+            }
+
+            //Activate UI
+            SignInPanel.Visible = false;
+            ChatPanel.Visible = true;
+            ButtonSend.Enabled = true;
+            TextBoxMessage.Focus();
+            RichTextBoxConsole.AppendText("Connected to server at " + ServerURI + Environment.NewLine);
+        }
+
+        /// <summary>
+        /// If the server is stopped, the connection will time out after 30 seconds (default), and the 
+        /// Closed event will fire.
+        /// </summary>
+        private void Connection_Closed(Exception e)
+        {
+            //Deactivate chat UI; show login UI. 
+            this.Invoke((Action)(() => ChatPanel.Visible = false));
+            this.Invoke((Action)(() => ButtonSend.Enabled = false));
+            this.Invoke((Action)(() => StatusText.Text = "You have been disconnected."));
+            this.Invoke((Action)(() => SignInPanel.Visible = true));
+        }
+
+        private void SignInButton_Click(object sender, EventArgs e)
+        {
+            UserName = UserNameTextBox.Text;
+            //Connect to server (use async method to avoid blocking UI thread)
+            if (!String.IsNullOrEmpty(UserName))
+            {
+                StatusText.Visible = true;
+                StatusText.Text = "Connecting to server...";
+                ConnectAsync();
+            }
+        }
+
+        private void WinFormsClient_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (Connection != null)
+            {
+                Connection.StopAsync().Wait();
+                Connection.DisposeAsync().Wait();
+            }
+        }
+    }
+}
