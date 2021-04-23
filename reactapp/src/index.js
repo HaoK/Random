105a3f427c55e293009e512a7be6fc47e5288c70
Hao Kung <HaoK@users.noreply.github.com>
2021-04-23 12:54:57 -0700

    Add initial angular app

--- reactapp/src/index.js
+++ reactapp/src/index.js
@@ -1,17 +1,25 @@
+import 'bootstrap/dist/css/bootstrap.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+//import registerServiceWorker from './registerServiceWorker';
+
+const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href');
+const rootElement = document.getElementById('root');
 
 ReactDOM.render(
-  <React.StrictMode>
+  <BrowserRouter basename={baseUrl}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+  </BrowserRouter>,
+  rootElement);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// Uncomment the line above that imports the registerServiceWorker function
+// and the line below to register the generated service worker.
+// By default create-react-app includes a service worker to improve the
+// performance of the application by caching static assets. This service
+// worker can interfere with the Identity UI, so it is
+// disabled by default when Identity is being used.
+//
+//registerServiceWorker();
+
