098b33cfb79105e0a4553e029e325cd1693ba9cd
Hao Kung <HaoK@users.noreply.github.com>
2021-04-27 10:03:25 -0700

    Fix e2e

--- angularapp/e2e/protractor.conf.js
+++ angularapp/e2e/protractor.conf.js
@@ -15,7 +15,6 @@ exports.config = {
     browserName: 'chrome'
   },
   directConnect: true,
-  SELENIUM_PROMISE_MANAGER: false,
   baseUrl: 'http://localhost:4200/',
   framework: 'jasmine',
   jasmineNodeOpts: {
@@ -25,7 +24,7 @@ exports.config = {
   },
   onPrepare() {
     require('ts-node').register({
-      project: require('path').join(__dirname, './tsconfig.e2e.json')
+      project: require('path').join(__dirname, './tsconfig.json')
     });
     jasmine.getEnv().addReporter(new SpecReporter({
       spec: {
