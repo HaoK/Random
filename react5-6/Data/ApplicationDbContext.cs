b48b5d81eb6cab5799224fe1039721fddd093d4b
Hao Kung <HaoK@users.noreply.github.com>
2021-05-06 11:52:01 -0700

    Add current react ind

---
+++ react5-6/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using react5_6.Models;
+using IdentityServer4.EntityFramework.Options;
+using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace react5_6.Data
+{
+    public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(
+            DbContextOptions options,
+            IOptions<OperationalStoreOptions> operationalStoreOptions) : base(options, operationalStoreOptions)
+        {
+        }
+    }
+}
